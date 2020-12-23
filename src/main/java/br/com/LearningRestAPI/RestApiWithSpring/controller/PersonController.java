178cfecff3fc7af9b9ce3419b1a0d74fb298291f
Edson Junior <edsonjos61@gmail.com>
2020-12-23 16:58:43 -0300

    Basic strutures for Get,Post,Put,Delete methods.

--- src/main/java/br/com/LearningRestAPI/RestApiWithSpring/controller/PersonController.java
+++ src/main/java/br/com/LearningRestAPI/RestApiWithSpring/controller/PersonController.java
@@ -1,8 +1,11 @@
 package br.com.LearningRestAPI.RestApiWithSpring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +20,32 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 	
+	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	public List<Person> findAll(){
+		return service.findAll();
+	}
+	
 	@RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
 	public Person findById(@PathVariable("id") String id){
 		return service.findById(id);
 	}
 	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+	public Person create(@RequestBody Person person){
+		return service.create(person);
+	}
+	
+	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
+	public Person update(@RequestBody Person person){
+		return service.update(person);
+	}
+	
+	@RequestMapping(value = "/{id}", 
+			method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id){
+		service.delete(id);
+	}
 	
 }
--- src/main/java/br/com/LearningRestAPI/RestApiWithSpring/services/PersonServices.java
+++ src/main/java/br/com/LearningRestAPI/RestApiWithSpring/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.LearningRestAPI.RestApiWithSpring.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -21,6 +23,37 @@ public class PersonServices {
 		person.setAddress("Rua 1");
 		person.setGender("Male");
 		return person ;
+	}
+	
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Address " + i);
+		person.setGender("Male");
+		return person;
 	}
 	
 	
