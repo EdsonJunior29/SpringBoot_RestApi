b98814efaa2675c9f9808f49eff387aca6f2f2c3
Edson Junior <edsonjos61@gmail.com>
2020-12-24 12:18:12 -0300

    Refactoring the HTTP verbs.

--- src/main/java/br/com/LearningRestAPI/RestApiWithSpring/controller/PersonController.java
+++ src/main/java/br/com/LearningRestAPI/RestApiWithSpring/controller/PersonController.java
@@ -3,11 +3,14 @@ package br.com.LearningRestAPI.RestApiWithSpring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.LearningRestAPI.RestApiWithSpring.model.Person;
@@ -20,32 +23,30 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 	
-	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	@GetMapping
 	public List<Person> findAll(){
 		return service.findAll();
 	}
 	
-	@RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	@GetMapping("/{id}")
 	public Person findById(@PathVariable("id") Long id){
 		return service.findById(id);
 	}
 	
-	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
-			produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+	@PostMapping
 	public Person create(@RequestBody Person person){
 		return service.create(person);
 	}
 	
-	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
-			produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.PUT)
+	@PutMapping
 	public Person update(@RequestBody Person person){
 		return service.update(person);
 	}
 	
-	@RequestMapping(value = "/{id}", 
-			method = RequestMethod.DELETE)
-	public void delete(@PathVariable("id") Long id){
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id){
 		service.delete(id);
+		return ResponseEntity.ok().build();
 	}
 	
 }
