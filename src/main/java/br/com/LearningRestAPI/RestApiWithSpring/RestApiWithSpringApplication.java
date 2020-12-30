d33d97608d25272be4e625923a43f1c7d0bf2eb8
Edson Junior <edsonjos61@gmail.com>
2020-12-29 21:27:25 -0300

    Adding Hateoas.

--- src/main/java/br/com/LearningRestAPI/RestApiWithSpring/RestApiWithSpringApplication.java
+++ src/main/java/br/com/LearningRestAPI/RestApiWithSpring/RestApiWithSpringApplication.java
@@ -1,9 +1,13 @@
 package br.com.LearningRestAPI.RestApiWithSpring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class RestApiWithSpringApplication {
 
 	public static void main(String[] args) {
